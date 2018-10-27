0b1e95da59bfb99793bdfbe252b1e4d9cbbc1034
Steve Snyder <ssnyde@gmail.com>
2018-10-26 21:21:10 -0400

    update bsp hw makefile to not auto generate boot scripts, and to create sd card image for boot from tftp and nfs. dockerfile is using debian due to ubuntu issue with quartus install hanging, have both quartus and soc eds installing

---
+++ docker/download_altera.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget http://download.altera.com/akdlm/software/acdsinst/17.1std/590/ib_installers/QuartusLiteSetup-17.1.0.590-linux.run 
+# wget http://download.altera.com/akdlm/software/acdsinst/17.1std/590/ib_installers/ModelSimSetup-17.1.0.590-linux.run 
+wget http://download.altera.com/akdlm/software/acdsinst/17.1std/590/ib_installers/cyclonev-17.1.0.590.qdz 
+
+# Integrated package with device support
+# wget http://download.altera.com/akdlm/software/acdsinst/17.1std/590/ib_tar/Quartus-lite-17.1.0.590-linux.tar
+
+# SoC EDS, needed for software support of Cyclone V HPS
+wget http://download.altera.com/akdlm/software/acdsinst/17.1std/590/ib_installers/SoCEDSSetup-17.1.0.590-linux.run
