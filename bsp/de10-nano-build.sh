0790031992f08d71038c21d481dea2aaf4cc8cfa
Steve Snyder <ssnyde@gmail.com>
2018-10-31 13:50:49 -0400

    update makefile to have fat_support for preloader enabled.  fill out build script to have hw/sw options.

--- bsp/de10-nano-build.sh
+++ bsp/de10-nano-build.sh
@@ -14,19 +14,31 @@ copy_to_server() {
     sudo tar -xf de10-nano-software/buildroot/output/images/rootfs.tar --directory /srv/nfs/opente_rootfs/
     echo "${green}Copy complete${reset}"
 }
-if [ "$1" == "first" ]; then
-    echo first
-fi
-if [ "$1" == "all" ]; then
+
+build_hw() {
     echo -e "${green}Building hardware${reset}"
     cd de10-nano-hardware
     make all
     cd ..
-    cd de10-nano-software
+}
+
+build_sw() {
     echo -e "${green}Building software${reset}"
+    cd de10-nano-software
     ./build.sh
     cd ..
+}
+
+if [ "$1" == "all" ]; then
+    build_hw
+    build_sw
     copy_to_server
+fi
+if [ "$1" == "hw" ]; then
+    build_hw
+fi
+if [ "$1" == "sw" ]; then
+    build_sw
 fi
 if [ "$1" == "copy_to_server" ]; then
     copy_to_server
